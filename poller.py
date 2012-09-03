1b0428aaf77f1c6eadfb6b20611a2e2e6f30fbce
Juho Vepsäläinen <bebraw@gmail.com>
2012-09-03 22:00:57 +0300

    Add initial support for growl
    If growl lib isn't available, prints to console instead.

--- poller.py
+++ poller.py
@@ -2,24 +2,40 @@
 import urllib2
 import ssl
 
+try:
+    import gntp.notifier as notify
+except ImportError:
+    notify = None
 
-def poll(sites, timeout):
+def poll(sites, timeout, ok, error):
     for site in sites:
-        print 'polling ' + site
+        ok('polling ' + site)
 
         try:
             response = urllib2.urlopen(site, timeout=timeout)
             response.read()
         except urllib2.URLError as e:
-            print e.code
+            error(site + ' ' + e.code)
         except ssl.SSLError as e:
-            print e.message
+            error(site + ' ' + e.message)
         else:
-            print 'ok'
+            ok('ok')
+
+
+def empty(data):
+    pass
+
+
+def output(data):
+    if notify:
+        notify.mini(data)
+    else:
+        print data
+
 
 if __name__ == '__main__':
     poll(sites=(
         'https://redmine.codegrove.org',
         'http://koodilehto.fi',
         'http://vakiopaine.net',
-        ), timeout=5)
+        ), timeout=5, ok=empty, error=output)
