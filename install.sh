3a73e97d106b23793c5c32e43a43d3e95ee8e362
Tuomo Sipola <tuomo.sipola@iki.fi>
2012-09-03 22:28:53 +0300

    Crontab to send output to /dev/null

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@
 CRON_TIME="@hourly"
 BIN_DIR="/usr/bin/"
 PROGRAM="check_websites"
+PARAMS=" > /dev/null"
 
 # Copy the program.
 sudo cp $PROGRAM $BIN_DIR$PROGRAM
 
-TO_CRON="$CRON_TIME $BIN_DIR$PROGRAM"
+TO_CRON="$CRON_TIME $BIN_DIR$PROGRAM $PARAMS"
 
 # Add to user's cronjobs.
 (crontab -l; echo "$TO_CRON") | crontab -
