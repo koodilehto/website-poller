11d8c670e4a00faf99b6ef8971cd3a3ebef5b634
Tuomo Sipola <tuomo.sipola@iki.fi>
2012-09-03 22:19:56 +0300

    Install script uses more variables

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 
 # Adds the program to user's cronjobs.
 
+CRON_TIME="@hourly"
 BIN_DIR="/usr/bin/"
 PROGRAM="check_websites"
 
 # Copy the program.
 sudo cp $PROGRAM $BIN_DIR$PROGRAM
 
-TO_CRON="/usr/bin/$PROGRAM"
+TO_CRON="$CRON_TIME $BIN_DIR$PROGRAM"
 
 # Add to user's cronjobs.
 (crontab -l; echo "$TO_CRON") | crontab -
